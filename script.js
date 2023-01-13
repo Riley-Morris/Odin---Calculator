906913c8e6001608b221f280a398d94c742756bf
Riley.Morris <rileymichaelmorris@gmail.com>
2023-01-13 14:03:56 -0500

    create index file, some basic operator functions

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+const tester = [7, 3, 1, 5]
+
+let currentOperator 
 function updateDisplay(currentValue){
     // do stuff here
     return 1
 }
 
 function addFunc(inpArray){
+    currentOperator = '+'
     return inpArray.reduce((accumulator, current)=> accumulator + current)
 }
 
 function subFunc (inpArray){
+    currentOperator = '-'
     return inpArray.reduce((accumulator, current) => accumulator - current)
 };
 
 function multFunc (inpArray){
+    currentOperator = '*'
     return inpArray.reduce((accumulator, current) => accumulator * current)
 }
 
 function divFunc (inpArray){
+    currentOperator = '/'
     return inpArray.reduce((accumulator, current) => accumulator/current)
 }
+
+// function equalButton(operator)
+
+console.log(divFunc(tester))
+
+
+console.log(currentOperator)
