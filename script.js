c5bfc2996c2c5afecaeaf0a2bc1de2c15dce61d6
Riley.Morris <rileymichaelmorris@gmail.com>
2023-01-19 11:30:45 -0500

    fix functions to properly concatenate numbers when attempting to make more than a 9 after operatorHandler is pressed

--- script.js
+++ script.js
@@ -16,19 +16,22 @@ function updateDisplay(numValue){
 }
 // handdle clicks of numbers, will concatenate number if no operator
 //is currently selected or add to number storage
+//toggler set to 1 to check if user wants to create something larger than 9
 function numClickHandler(numValue) {
     // if trying to combine 1 digit numbers to make something larger than 9
-    if (numberStorage.length > 0) {
-        numberStorage[0] = parseFloat(numberStorage[0].toString + numValue)
+    if (numberStorage.length > 0 && toggler === 1) {
+        numberStorage[numberStorage.length - 1] = parseFloat(numberStorage[numberStorage.length - 1].toString() + numValue.toString())
     
     }else {
         numberStorage.push(numValue)
+        toggler = 1
     }
 }
 
 //operator button func
 //check whether there are already 2 nums in storage and will operate if clicked
 function operatorHandler(operatorValue){
+    toggler = 0
     if (operatorStorage === '') {
         operatorStorage = operatorValue
     } else {
